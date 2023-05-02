e65a54654d4a0c85c8463a8a0820b082bf6473c8
Bas Lijten <bas.lijten@gmail.com>
2023-05-02 09:08:49 +0200

    reverted change for title

--- gatsby-config.js
+++ gatsby-config.js
@@ -202,7 +202,6 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  title: edge.node.title + " - Bas Lijten",
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,

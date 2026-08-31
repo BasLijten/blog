77cab15a98b33a54b8e6303fb19ef1ff074cfb31
Bas Lijten <bas.lijten@gmail.com>
2026-08-31 16:08:05 +0200

    feat: add recovery links to the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import DefaultLayout from '../components/layout'
 import SEO from '../components/seo'
+import { Link } from 'gatsby'
 
 class NotFoundPage extends React.Component {
   render() {
@@ -11,7 +12,12 @@ class NotFoundPage extends React.Component {
       <DefaultLayout>
         <SEO title="404: Not Found" />
         <h1>That page doesn&#39;t exist.</h1>
-        <br />
+        <p>
+          The requested page could not be found. Start at the{' '}
+          <Link to="/">homepage</Link>, browse the <Link to="/tags">tags</Link>,
+          or use the <a href="/sitemap-index.xml">sitemap</a> and{' '}
+          <a href="/llms.txt">agent guide</a> to find another page.
+        </p>
       </DefaultLayout>
     )
   }
